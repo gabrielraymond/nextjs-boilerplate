7ca1bb49429d48a95c4d78ba68443b6634cbe880
Gabriel Raymond <gabrielraymond.grd@gmail.com>
2025-02-22 17:50:55 +0700

    feat: svg icon component

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import MyIcon from "./MyIcon";
+import ButtonBurger from "./ButtonBurger";
 
 const Navbar = () => {
   return (
-    <nav className='w-full bg-green-600 h-16 text-white'>asdfasdfasdfasdfsadfsa</nav>
-  )
-}
+    <nav className="w-full bg-green-600 h-16 text-white flex items-center px-8">
+      <div>
+        <ButtonBurger />
+        <div></div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
